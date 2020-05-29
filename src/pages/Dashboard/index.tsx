9332080c508ebff90ee55dc4a226c9cdd862068f
Beatriz Murase <biamurase@gmail.com>
2020-05-29 16:01:37 -0300

    feat: add loadFoods

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,12 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      // api.get('/foods').then(response => {
+      //   setFoods(response.data);
+      // });
+
+      setFoods(response.data);
     }
 
     loadFoods();
