7302e34d6653dca0f0c91b6c27542a1a7a6b0a5d
Beatriz Murase <biamurase@gmail.com>
2020-05-29 17:51:49 -0300

    feat: toggleAvailable

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
+import api from '../../services/api';
 
 interface IFoodPlate {
   id: number;
@@ -27,6 +28,13 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
+    const updatedFood = {
+      ...food,
+      available: !isAvailable,
+    };
+
+    await api.put(`/foods/${food.id}`, updatedFood);
+
     setIsAvailable(!isAvailable);
   }
 
